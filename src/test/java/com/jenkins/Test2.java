6ebf52eae58c18ad907cc7cc88f7a33c0b5c3cdc
Balahere <balakrishnasolium@gmail.com>
2018-07-21 22:02:09 +0530

    added tutumon

---
+++ src/test/java/com/jenkins/Test2.java
@@ -0,0 +1,16 @@
+package com.jenkins;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+		public class Basic {
+
+		@Test
+		public void display()
+		{
+			System.out.println("++++++++++++++++++++++++++++++++++TutuMOn is sleepy++++++++++++++++++++++++++++++");
+		}
+	}
+
+}
